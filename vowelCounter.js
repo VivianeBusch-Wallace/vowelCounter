2731cb2108e03c078e156e8321ecbc4f745efcb2
viviane <v.buschwallace@yahoo.com>
2021-02-16 14:30:57 +0100

    best version started

---
+++ vowelCounter.js
@@ -0,0 +1,26 @@
+console.log(
+  "-----------How many vowels does your word or sentence have?------------"
+);
+// #### 1. How many?
+// * Create a function named "_vowelCount_" that accepts a string as an argument.
+// Check how many vowels the string contains, if any.
+// Return the vowel count of the string.
+//     > **Examples**:
+//     - ```javascript
+//       vowelCount("hello") // 2
+//       ```
+//     - ```javascript
+//       vowelCount("test") // 1
+//       ```
+//     - ```javascript
+//       vowelCount("fbw") // 0
+//       ```
+const vowelCount = (str) => {
+  const strArr = str.toLowerCase().split("");
+
+  return counter;
+};
+console.log(vowelCount("hello"));
+console.log(vowelCount("test"));
+console.log(vowelCount("fbw"));
+console.log(vowelCount("wie alt bist du?"));
